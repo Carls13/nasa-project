ce6199ed83cf762f2db01da7ceea06ec6392b80f
Carlos Hernández <charlesshb98@gmail.com>
2023-09-15 19:00:00 -0400

    fix: config for tests and moved mongo db connecting functions

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,15 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
+const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.port || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log("MongoDB connection ready!")
-});
-
-mongoose.connection.on('error', (error) => {
-    console.error(error);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    await mongoConnect();
     await loadPlanetsData();
 
     server.listen(PORT, () => {
