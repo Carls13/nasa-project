9d8652a568d3d9f71747be24f32d35aea4ac3c6d
Carlos Hernández <charlesshb98@gmail.com>
2023-09-16 20:31:37 -0400

    config: versioning API

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
+const api = require('./api');
 require('dotenv').config();
-
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -17,8 +15,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
     res.sendFile((path.join(__dirname, '..', 'public', 'index.html')));
--- server/src/server.js
+++ server/src/server.js
@@ -7,17 +7,7 @@ const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.port || 8000;
 
-const MONGO_URL = 'mongodb+srv://charlesshb98:OGizEd8RoaVRiO03@cluster0.fsfa1n4.mongodb.net/nasa-db?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
-
-mongoose.connection.once('open', () => {
-    console.log("MongoDB connection ready!")
-});
-
-mongoose.connection.on('error', (error) => {
-    console.error(error);
-});
 
 async function startServer() {
     await mongoConnect();
